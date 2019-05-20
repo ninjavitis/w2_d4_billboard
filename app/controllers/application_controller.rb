b97aa82a3f27d2da727e11ded63b57644b4c3029
Peter Mackay <project2501@gmail.com>
2019-05-20 14:42:32 -0600

    added devise user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :sanitize, if: :devise_controller?
+
+  protected
+  def sanitize
+    devise_parameter_sanitizer.permit(:route, keys:[:first_name, :last_name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "song_boards#index"
   get 'static_pages/memorial'
   get '/song_boards/:id/songs', to: 'songs#board_index'
+  devise_for :users
 
 
   resources :song_boards do
